c342b42d5bc3c14253dcf98ba59b2a390ea0cace
Bui Thanh Lam <lamthientaihehe@gmail.com>
2020-11-24 13:24:55 +0700

    refactor code

--- WMF.py
+++ WMF.py
@@ -1,115 +1,8 @@
 import numpy as np
 import pandas as pd
 import os.path
-import normalize
+import utils
 
-
-class IFConverter:
-    """
-    Convert triplets data (user_id, song_id, play_count) into R, P, C matrices
-    Arguments:
-        - path: path to triplets data
-        - alpha (optional): penalty for confidence. Default: 40
-    Methods:
-        - get_implicit_feedback(): read triplets and calculate R. Return None
-        - convert(): convert R into P and C for weighted matrix factorization. Return None
-        - save() and load(): save and load R, P, C if they have been calculated already. Return None
-        - get_dictionary(): Return mapping of users and items
-    """
-    def __init__(self, path, alpha=40):
-        self.path = path
-        self.n_users = 0
-        self.n_items = 0
-        self.R = None
-        self.P = None
-        self.C = None
-        self.alpha = alpha
-        self.dict_user = {}
-        self.dict_items = {}
-
-    def get_implicit_feedback(self):
-        """Return R matrix"""
-        data = pd.read_csv(self.path)
-        users = data['User'].unique()
-        items = data['Song'].unique()
-
-        # list user
-        id = 0
-        for user in users:
-            self.dict_user[id] = user
-            self.dict_user[user] = id
-            id += 1
-
-        # list items
-        id = 0
-        for item in items:
-            self.dict_items[id] = item
-            self.dict_items[item] = id
-            id += 1
-        # make R matrix
-        self.n_users = int(len(self.dict_user) / 2)
-        self.n_items = int(len(self.dict_items) / 2)
-        self.R = np.zeros((self.n_users, self.n_items), dtype=float)
-        for index, row in data.iterrows():
-            self.R[self.dict_user[row['User']], self.dict_items[row['Song']]] = float(row['Play count'])
-
-    def convert(self):
-        """Convert R into P and C"""
-        self.P = np.zeros_like(self.R)
-        self.C = np.ones_like(self.R)
-        for i in range(self.n_users):
-            for j in range(self.n_items):
-                if self.R[i, j] == 0:
-                    continue
-                self.P[i, j] = 1
-                self.C[i, j] = 1 + self.alpha * self.R[i, j]
-
-    def save_R_real(self):
-        np.savetxt('./data/R-real.txt', self.R, delimiter=' ', fmt='%d')
-        with open('./data/dict-user.txt', 'w') as f:
-            for i in range(self.n_users):
-                f.write(str(i) + ' ' + self.dict_user[i])
-                f.write('\n')
-
-        with open('./data/dict-item.txt', 'w') as f:
-            for i in range(self.n_items):
-                f.write(str(i) + ' ' + self.dict_items[i])
-                f.write('\n')
-
-    def load_R_train(self):
-        with open('./data/R-train.txt', 'r') as f:
-            self.R = [[float(num) for num in line[:-1].split(' ')] for line in f]
-            self.R = np.array(self.R)
-            self.n_users = self.R.shape[0]
-            self.n_items = self.R.shape[1]
-
-    def save(self):
-        if self.P is not None:
-            np.savetxt('./data/P.txt', self.P, delimiter=' ', fmt='%d')
-        if self.C is not None:
-            np.savetxt('./data/C.txt', self.C, delimiter=' ', fmt='%.3f')
-
-    def load(self):
-        if os.path.isfile('./data/P.txt'):
-            with open('./data/P.txt', 'r') as f:
-                self.P = [[float(num) for num in line[:-1].split(' ')] for line in f]
-                self.P = np.array(self.P)
-        if os.path.isfile('./data/C.txt'):
-            with open('./data/C.txt', 'r') as f:
-                self.C = [[float(num) for num in line[:-1].split(' ')] for line in f]
-                self.C = np.array(self.C)
-
-    def get_dictionary(self):
-        dict_items = {}
-        dict_users = {}
-        with open('./data/dict-user.txt', 'r') as f:
-            for line in f:
-                dict_users[line.split(' ')[0]] = line.split(' ')[1]
-
-        with open('./data/dict-item.txt', 'r') as f:
-            for line in f:
-                dict_items[line.split(' ')[0]] = line.split(' ')[1]
-        return dict_items, dict_users
 
 class WeightedMF:
     """
@@ -129,7 +22,7 @@ class WeightedMF:
         - predict(): return list of recommendations for a user given the user_id and number of items to recommend
         - save(), load(): save and load U, I if they have been calculated already
     """
-    def __init__(self, P, C, dict_user, dict_item, depth=5, n_epoches=200, lr=1e-6, rgl=0.02, graph_inferred=False, early_stopping=True, verbose=False):
+    def __init__(self, P, C, dict_user, dict_item, depth=5, n_epoches=200, lr=1e-6, rgl=0.02, batch_size=0, graph_inferred=False, early_stopping=True, verbose=False):
         self.P = P
         self.C = C
         self.n_users = P.shape[0]
@@ -146,8 +39,10 @@ class WeightedMF:
         self.graph_inferred = graph_inferred
         self.verbose = verbose
         self.early_stopping = early_stopping
+        if batch_size == 0:
+            self.batch_size = int(self.n_items * self.n_users / 1000)
 
-    def gradient_descent(self):
+    def __gd(self):
         E_U = np.ones((1, self.depth), dtype=float)
         E_i = np.ones((self.depth, 1), dtype=float)
         dU = np.zeros_like(self.U)
@@ -170,10 +65,10 @@ class WeightedMF:
 
         return dU, dI
 
-    def stochastic_gradient_descent(self):
+    def __sgd(self):
         E_U = np.ones((1, self.depth), dtype=float)
         E_i = np.ones((self.depth, 1), dtype=float)
-        batch_size = int(self.n_items * self.n_users / 1000)
+
         dU = np.zeros_like(self.U)
         dI = np.zeros_like(self.I)
         X = []
@@ -181,7 +76,7 @@ class WeightedMF:
         mark = np.zeros_like(self.C)
 
         index = 0
-        while index < batch_size:
+        while index < self.batch_size:
             x = np.random.randint(self.n_users)
             y = np.random.randint(self.n_items)
             if mark[x, y] == 0:
@@ -189,7 +84,7 @@ class WeightedMF:
                 X.append(x)
                 Y.append(y)
                 index += 1
-        for u in range(batch_size):
+        for u in range(self.batch_size):
             i = X[u]
             j = Y[u]
             dU[i] += 2 * self.C[i, j] * (np.dot(self.U[i, :], self.I[:, j]) - self.P[i, j]) \
@@ -217,37 +112,38 @@ class WeightedMF:
             if self.early_stopping and current_epoch > 1 and (prev_loss - loss < 0.001):
                 return None
             else:
-                #dU, dI = self.gradient_descent()
-                dU, dI = self.stochastic_gradient_descent()
+                dU, dI = self.__gd()
+                # dU, dI = self.__sgd()
                 self.U -= self.lr * dU
                 self.I -= self.lr * dI
 
-        # Normalize 0~1
-        for i in range(self.n_users):
-            self.U[i] = normalize.softmax_normalize(self.U[i])
-        for i in range(self.depth):
-            self.I[i] = normalize.softmax_normalize(self.I[i])
-        self.predict = np.dot(self.U, self.I)
+        # # Normalize 0~1
+        # for i in range(self.n_users):
+        #     self.U[i] = utils.softmax_normalize(self.U[i])
+        # for i in range(self.depth):
+        #     self.I[i] = utils.softmax_normalize(self.I[i])
+        # self.predict = np.dot(self.U, self.I)
 
-    def predict(self, user_index, n_rec_items):
+    def get_recommendations(self, user_index, n_rec_items):
         recommendations = np.argsort(self.predict[user_index])
         recommendations = recommendations[-n_rec_items:]
-        name_of_songs_rec = []
-        for i in range(n_rec_items):
-            name_of_songs_rec.append(self.dict_item[recommendations[i]])
-        return name_of_songs_rec, recommendations
+        # name_of_songs_rec = []
+        # for i in range(n_rec_items):
+        #     name_of_songs_rec.append(self.dict_item[recommendations[i]])
+        # return name_of_songs_rec
+        return recommendations
 
-    def cal_recall(self, user_index):
-        sum_of_deleted_feedback = 0
-        true_positive = 0
-        name_of_songs_res, recommendations = self.predict()
-        R_train, R_full = self.load_train_file()
-        for i in range(self.n_items):
-            if R_train[user_index, i] != R_full[user_index, i]:
-                sum_of_deleted_feedback += 1
-                if i in recommendations:
-                    true_positive += 1
-        return float(true_positive/sum_of_deleted_feedback) * 100
+    # def evaluate(self, user_index):
+    #     sum_of_deleted_feedback = 0
+    #     true_positive = 0
+    #     name_of_songs_res, recommendations = self.predict()
+    #     R_train, R_full = self.load_train_file()
+    #     for i in range(self.n_items):
+    #         if R_train[user_index, i] != R_full[user_index, i]:
+    #             sum_of_deleted_feedback += 1
+    #             if i in recommendations:
+    #                 true_positive += 1
+    #     return float(true_positive/sum_of_deleted_feedback) * 100
 
     def save(self):
         np.savetxt('./data/U.txt', self.U, delimiter=' ', fmt='%.5f')
@@ -264,12 +160,3 @@ class WeightedMF:
                 self.I = [[float(num) for num in line[:-1].split(' ')] for line in f]
                 self.I = np.array(self.I)
 
-    def load_train_file(self):
-        with open('./data/R-train.txt', 'r') as f:
-            R_train = [[float(num) for num in line[:-1].split(' ')] for line in f]
-            R_train = np.array(R_train)
-        with open('./data/R-real.txt', 'r') as f:
-            R_full = [[float(num) for num in line[:-1].split(' ')] for line in f]
-            R_full = np.array(R_full)
-
-        return R_train, R_full
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from WMF import WeightedMF
-from WMF import IFConverter
+from IFConverter import IFConverter
+import os
+import numpy as np
 
-converter = IFConverter('./data/TEST.csv')
-# Loading fake data. TEST.csv and this R.txt are irrelevant.
-#converter.get_implicit_feedback()
-#converter.save_R_real()
-#converter.load_R_train()
-#converter.convert()
-#converter.save()
-converter.load()
+converter = IFConverter(alpha=10)
+# converter.get_implicit_feedback('./data/TEST.csv')
+# Loading fake data
+with open('./data/R-train.txt', 'r') as f:
+    converter.R = [[float(num) for num in line[:-1].split(' ')] for line in f]
+    converter.R = np.array(converter.R)
+converter.convert()
 P, C = converter.P, converter.C
 dict_item, dict_user = converter.get_dictionary()
 
 wmf = WeightedMF(P, C, dict_user, dict_item, depth=5, early_stopping=True, verbose=True)
 wmf.fit()
-wmf.save()
+print(wmf.get_recommendations(5, 20))
+# Compare to original data
+with open('./data/R-full.txt', 'r') as f:
+    origin = [[float(num) for num in line[:-1].split(' ')] for line in f]
+    origin = np.array(origin)
+recommendations = np.argsort(origin[5])
+recommendations = recommendations[-20:]
+print(recommendations)
